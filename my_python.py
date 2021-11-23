07cb069ba195506c00bac76d82814808952ccc6c
Aleksey <79donaldson@gmail.com>
2021-11-23 13:50:32 +0200

    add changes script py

--- my_python.py
+++ my_python.py
@@ -3,6 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return 'Hello, this is my first application!' 
+
+@app.route('/')
+def template():
     return render_template('index.html')
 
 if __name__ == '__main__':
